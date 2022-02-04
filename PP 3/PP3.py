512c7e8360df632450c2ab8a2c3c512f6189df1b
ppcf8 <98133959+ppcf8@users.noreply.github.com>
2022-02-04 14:29:08 +0000

    Add files via upload

---
+++ PP 3/PP3.py
@@ -0,0 +1,21 @@
+# PP3 - using BFGS method
+# Pedro Ferreira
+
+from scipy.optimize import minimize
+import numpy as np
+
+def objective(x):
+    return (x[0]-x[1]-2*x[0]*x[1]-x[0]**2-2*x[1]**2)*-1
+
+
+def rosen_der(x):
+    der = np.zeros_like(x)
+    der[0] =-(1-2*x[1]-2*x[0])
+    der[1] = -(-1-2*x[0]-4*x[1])
+    return der
+
+x0=[0,0]
+
+sol = minimize(objective,x0,method='BFGS',jac=rosen_der,options={'disp': True})
+print(sol)
+
