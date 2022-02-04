624965b5a0a72128435ee0565e699632b4be16a5
ppcf8 <98133959+ppcf8@users.noreply.github.com>
2022-02-04 14:36:20 +0000

    Delete PP 5 directory

--- PP 5/PP5_search.m
+++
@@ -1,8 +0,0 @@
-%%                  Exercise PP 1 - Search Direction
-%--------------------------------------------------------------------------
-% Computes the search direction as defined in the Steepest Descent Method.
-%
-% Pedro Ferreira 2021
-%--------------------------------------------------------------------------
-
-d=-grad_f(x(1),x(2));
